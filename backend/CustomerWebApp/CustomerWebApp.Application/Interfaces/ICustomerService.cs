03741ca5a563fd9b824d08b93c494fdcc95e2e63
EMMANUEL EGITO OLIVEIRA CANDEIRA MARINHO <e_egito@hotmail.com>
2022-05-03 14:35:17 -0300

    criação das camadas de container, aplicação e inserção das migrações para o banco

---
+++ backend/CustomerWebApp/CustomerWebApp.Application/Interfaces/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using CustomerWebApp.Application.DTOs;
+
+namespace CustomerWebApp.Application.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task AddCustomer(CustomerDTO customerDTO);
+        Task<IEnumerable<CustomerDTO>> GetAllCustomers();
+        Task<CustomerDTO> GetCustomerByCpf(string cpf);
+    }
+}
---
+++ backend/CustomerWebApp/CustomerWebApp.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CustomerWebApp.Application.DTOs;
+using CustomerWebApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerWebApp.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Customer, CustomerDTO>().ReverseMap();
+        }
+    }
+}
